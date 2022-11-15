f4bd404b627b620baf3483a1d05d8f8b81be94a6
luanitachi <81259369+luanitachi@users.noreply.github.com>
2022-11-14 21:04:12 -0300

    adicionados via upload
    Tecnologias: HTML, CSS e JS

---
+++ script.js
@@ -0,0 +1,48 @@
+let imagem = document.querySelector(".starbucks");
+let cor = document.querySelector(".circle");
+
+
+function trocaImagem(endereco) {
+    imagem.src = endereco
+}
+
+function trocaCor(color) {
+    cor.style.backgroundColor = color;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function tuti() {                                                                tambem troca a cor e a iamgem
+//     document.getElementById("circulo").style.backgroundColor= "#eb7495";
+//     document.getElementById("milk").setAttribute("src", "img/morango.png")
+// }
+
+// function menta() {
+//     document.getElementById("circulo").style.backgroundColor= "#017143";
+//     document.getElementById("milk").setAttribute("src", "img/menta.png")
+// }
+
+// function morang() {
+//     document.getElementById("circulo").style.backgroundColor= "#d752b1";
+//     document.getElementById("milk").setAttribute("src", "img/tuti-fruti.png")
+// }
